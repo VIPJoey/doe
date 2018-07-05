ee70fbf535fc567cd7226e374fb2c59c2174e9ed
VIPJoey <624031372@qq.com>
2018-07-05 17:30:39 +0800

    首次发布

---
+++ mmc-dubbo-doe/src/test/java/com/mmc/dubbo/doe/test/TestTelnetService.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2010-2020 Founder Ltd. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Founder. You shall not disclose such Confidential Information
+ * and shall use it only in accordance with the terms of the agreements
+ * you entered into with Founder.
+ *
+ */
+package com.mmc.dubbo.doe.test;
+
+/**
+ * @author Joey
+ * @date 2018/7/5 14:57
+ */
+public class TestTelnetService {
+
+}
